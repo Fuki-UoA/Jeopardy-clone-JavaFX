6f3fed9f015e7007d6ea5d07194814d3fd00abff
Fuki-UoA <fbab920@aucklanduni.ac.nz>
2020-08-31 00:26:41 +1200

    Add package and main filie

---
+++ src/src/JeopardyGUI.java
@@ -0,0 +1,12 @@
+package src;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JeopardyGUI extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
