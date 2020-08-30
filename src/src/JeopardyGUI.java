7b7fcdb6b424e6993daf18cb4d55a1860333377c
Fuki-UoA <fbab920@aucklanduni.ac.nz>
2020-08-31 00:39:21 +1200

    Add Main file with template

--- src/src/JeopardyGUI.java
+++ src/src/JeopardyGUI.java
@@ -1,12 +1,25 @@
 package src;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class JeopardyGUI extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Jeopardy");
 
+        BorderPane pane = new BorderPane();
+
+        Scene scene = new Scene(pane, 1000, 800);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
