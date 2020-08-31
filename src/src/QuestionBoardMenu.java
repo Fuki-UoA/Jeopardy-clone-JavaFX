2700bb44da458c2674e6a213dfadf93cf5d5ec88
Fuki-UoA <fbab920@aucklanduni.ac.nz>
2020-08-31 22:54:57 +1200

    Add QuestionBoardMenu class

--- src/src/QuestionBoardMenu.java
+++ src/src/QuestionBoardMenu.java
@@ -1,2 +1,18 @@
-package src;public class QuestionBoardMenu {
+package src;
+
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class QuestionBoardMenu extends BorderPane {
+    Color _color;
+    public QuestionBoardMenu(Stage stage, Color color){
+        _color = color;
+        this.setBackground(new Background(new BackgroundFill(_color, CornerRadii.EMPTY, Insets.EMPTY)));
+
+    }
 }
