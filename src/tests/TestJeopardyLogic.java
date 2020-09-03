ce703187bfeaf38a9773244dde9a9a5caf112008
Fuki-UoA <fbab920@aucklanduni.ac.nz>
2020-09-03 23:44:29 +1200

    Add test class

--- src/tests/TestJeopardyLogic.java
+++ src/tests/TestJeopardyLogic.java
@@ -1,2 +1,20 @@
-package tests;public class TestJeopardyLogic {
+package tests;
+
+import org.junit.Test;
+import src.JeopardyLogic;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestJeopardyLogic {
+
+    @Test
+    public void testAnswers() throws IOException {
+        JeopardyLogic logic = new JeopardyLogic();
+        String[][] answers = logic.getAnswers();
+
+        assertEquals("Kiwi", answers[0][0]);
+    }
+
 }
