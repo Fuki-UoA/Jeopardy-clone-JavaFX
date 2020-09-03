5ea6c32d4a2b3f3c64f2fde620f72b56ada0d604
Fuki-UoA <fbab920@aucklanduni.ac.nz>
2020-09-03 21:12:13 +1200

    Add functionality to Logic class

--- src/src/JeopardyLogic.java
+++ src/src/JeopardyLogic.java
@@ -6,20 +6,72 @@ public class JeopardyLogic {
     private int _numOfQuestions;
     private int _numOfCategories;
     private int[] scores;
+    private int _winning;
     private String[][] questions;
     private String[][] answers;
+    private Boolean[][] isAnswered;
 
-    public JeopardyLogic(){
+    public JeopardyLogic() throws IOException {
         countCategories();
         countQuestions();
 
         scores = new int[_numOfQuestions];
         questions = new String[_numOfCategories][_numOfQuestions];
         answers = new String[_numOfCategories][_numOfQuestions];
+        isAnswered = new Boolean[_numOfCategories][_numOfQuestions];
 
+        for(int i = 0; i < _numOfQuestions; i++){
+            for(int j = 0; j < _numOfCategories; j++){
+                isAnswered[i][j] = false;
+            }
+        }
+
+        BufferedReader br = new BufferedReader(new FileReader(new File(".." + File.separator +"categories")));
+
+        int i = 0;
+        int j = 0;
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            BufferedReader in = new BufferedReader(new FileReader(new File(".." +
+                    File.separator +"categories" + File.separator + line)));
+
+            while((line = in.readLine()) != null){
+                String[] aLine = line.split(",");
+
+                scores[i] = Integer.parseInt(aLine[0]);
+                questions[i][j] = aLine[1];
+                answers[i][j] = aLine[2];
+
+                i++;
+            }
+            j++;
+        }
         
     }
 
+    public String[][] getAnswers(){
+        String[][] result = new String[_numOfQuestions][_numOfCategories];
+        for(int i = 0; i < _numOfQuestions; i++){
+            for (int j = 0; j < _numOfCategories; j++){
+                if(isAnswered[i][j] == true){
+                    questions[i][j] = "Answered";
+                } else {
+                    result[i][j] = questions[i][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int getWinning(){
+        return _winning;
+    }
+    
+    public void reset() {
+        _winning = 0;
+    }
 
     private int readFirstLine(ProcessBuilder p) throws IOException {
         ProcessBuilder pb = p;
@@ -37,7 +89,6 @@ public class JeopardyLogic {
         ProcessBuilder pb =
                 new ProcessBuilder("/bin/bash","-c", "ls | head -n1 | xargs cat | wc -l" );
         pb.directory(new File("../categories"));
-
 
         try {
             _numOfQuestions = readFirstLine(pb);
